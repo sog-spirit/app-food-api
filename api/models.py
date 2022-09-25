95f02bfa861c87a9a88b9a1d53cd25182d2191ec
sog-spirit <78286856+sog-spirit@users.noreply.github.com>
2022-09-25 21:11:55 +0700

    Register APIView created

--- api/models.py
+++ api/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=True)
     email = models.CharField(max_length=255, unique=True)
     username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     phone = models.CharField(max_length=15)
-    avatar = models.TextField()
-    address = models.TextField()
-    date_of_birth = models.DateField()
+    avatar = models.TextField(null=True)
+    address = models.TextField(null=True)
+    date_of_birth = models.DateField(null=True)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['password', 'email', 'phone']
