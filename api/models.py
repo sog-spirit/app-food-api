6088b47fb6d776521594797b64ce8e8e4459299c
sog-spirit <78286856+sog-spirit@users.noreply.github.com>
2022-10-04 00:26:35 +0700

    Custom django admin view added. Make name, avatar, address, date_of_birth can be blanked in django admin form. Test staff account added.

--- api/models.py
+++ api/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    name = models.CharField(max_length=255, null=True)
+    name = models.CharField(max_length=255, null=True, blank=True)
     email = models.CharField(max_length=255, unique=True)
     username = models.CharField(max_length=255, unique=True)
     password = models.CharField(max_length=255)
     phone = models.CharField(max_length=15)
-    avatar = models.TextField(null=True)
-    address = models.TextField(null=True)
-    date_of_birth = models.DateField(null=True)
+    avatar = models.TextField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True, blank=True)
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['password', 'email', 'phone']
