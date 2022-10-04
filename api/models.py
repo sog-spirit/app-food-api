80bb11637b91fd9d33ceb19b674138308254574d
sog-spirit <78286856+sog-spirit@users.noreply.github.com>
2022-10-04 20:02:44 +0700

    models.py modified. No migration has been made.

--- api/models.py
+++ api/models.py
@@ -14,3 +14,35 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = ['password', 'email', 'phone']
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    image = models.TextField(null=True, blank=True)
+    price = models.FloatField(default=0.0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    description = models.TextField()
+    quantity = models.IntegerField(default=0)
+
+class OrderDetail(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_details')
+    quantity = models.IntegerField(default=0)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='order_details')
+    
+class Order(models.Model):
+    price = models.FloatField(default=0.0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    payment_method =
+    order_status =
+
+class Review(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='reviews')
+    content = models.TextField()
+    rating = models.FloatField()
+
+class Coupon(models.Model):
+    discount = models.IntegerField(default=0)
+    name = models.CharField(max_length=255)
